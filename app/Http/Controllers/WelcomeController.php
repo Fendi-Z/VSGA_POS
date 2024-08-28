a77640fa17ca383f6597d63e0b1008bd942a549b
Fendi-Z <muhammadaffandi301@gmail.com>
2024-08-28 10:59:53 +0700

    Praktikum 1 : Layouting  AdminLTE P29

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Selamat Datang',
+            'list' => ['Home', 'Welcome']
+        ];
+
+        return view('welcome', ['breadcrumb' => $breadcrumb]);
+    }
+}
